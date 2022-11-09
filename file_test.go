17f49a5c82f09008f4e4c08b01cb98e3507c69fa
bmayer3 <bmayer@sibros.tech>
2022-11-08 23:11:19 -0300

    Adde mix test

---
+++ file_test.go
@@ -0,0 +1,18 @@
+package gwi
+
+import (
+	"testing"
+)
+
+func Test_Mix(t *testing.T) {
+	vault, err := NewFileVault("test.json", "----xxx----")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if res := vault.Mix("1234"); res != "759b18c03bf6df73ab38b70a553ccccbd86230f841abba5a6bd4b3b1d7a3937a" {
+		t.Error("mix(1234) is ", res)
+		return
+	}
+}
