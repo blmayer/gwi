cae4cb1b8f16c81bbaa096c84b9e2d90a79ffff8
blmayer <bleemayer@gmail.com>
2022-12-26 15:40:04 -0300

    Improved file count

--- tree.go
+++ tree.go
@@ -20,13 +20,13 @@ func (g *Gwi) file(repo *git.Repository) func(ref plumbing.Hash, name string) st
 		logger.Debug("getting file", name)
 		fileObj, err := commit.File(name)
 		if err != nil {
-			logger.Error("head file error:", err.Error())
+			logger.Error(name, "file error:", err.Error())
 			return ""
 		}
 
 		c, err := fileObj.Contents()
 		if err != nil {
-			logger.Error("file contents error:", err.Error())
+			logger.Error(name, "contents error:", err.Error())
 			return ""
 		}
 
@@ -44,15 +44,32 @@ func (g *Gwi) files(repo *git.Repository) func(ref plumbing.Hash) int {
 			return -1
 		}
 
-		logger.Debug("getting tree for commit", commit.Hash.String())
-		tree, err := commit.Tree()
+		logger.Debug("getting files for commit", commit.Hash.String())
+		t, err := commit.Tree()
 		if err != nil {
 			logger.Error("trees error:", err.Error())
 			return -1
 		}
 
-		return len(tree.Entries)
+		return countFiles(t)
 	}
+}
+
+func countFiles(t *object.Tree) int {
+	count := 0
+	for _, e := range t.Entries {
+		if e.Mode.IsFile() {
+			count += 1
+		} else {
+			t2, err := t.Tree(e.Name)
+			if err != nil {
+				logger.Error("tree tree", err.Error())
+				continue
+			}
+			count += countFiles(t2)
+		}
+	}
+	return count
 }
 
 func (g *Gwi) tree(repo *git.Repository) func(ref plumbing.Hash) []File {
